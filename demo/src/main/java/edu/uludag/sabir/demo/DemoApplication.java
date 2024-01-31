12f1fd8245d57e6739b70cc069e38f7478a37c13
Sabir Süleymanlı <suleymanlisabir3@gmail.com>
2024-01-31 13:54:35 +0300

    Create DemoApplication.java

--- demo/src/main/java/edu/uludag/sabir/demo/DemoApplication.java
+++ demo/src/main/java/edu/uludag/sabir/demo/DemoApplication.java
@@ -1,3 +1,15 @@
+/*
+ * File: DemoApplication.java
+ * Author: Sabir Süleymanlı  		(GitHub: @sabir-suleyman) (Mail: @suleymanlisabir3@gmail.com)
+ * Date: January 31, 2024
+ * Description: A Spring Boot application demonstrating the basic setup of a Spring Boot project. 
+ *              The main class, DemoApplication, is annotated with @SpringBootApplication, 
+ *              indicating that it is the entry point of the application and includes 
+ *              Spring Boot's auto-configuration features. The main method initializes the 
+ *              Spring application context and starts the application.
+ * 
+ */
+
 package edu.uludag.sabir.demo;
 
 import org.springframework.boot.SpringApplication;
